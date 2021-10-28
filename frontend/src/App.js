7637ed3c95c95294d5269add997ae78540985a3f
Santosh Nepal <santoshnepal608@gmail.com>
2021-10-28 16:01:06 +0545

    Registration Form Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import { NavBar } from './components/layout/NavBar';
-import { Landing } from './components/layout/Landing';
+import NavBar  from './components/layout/NavBar';
+import  Landing  from './components/layout/Landing';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import { BrowserRouter as Router , Route,Switch } from 'react-router-dom';
 import './App.css';
 import React , {Fragment} from 'react'
 const App = () => (
-  <Fragment>
+  <Router>
+    <Fragment>
     <NavBar/>
-    <Landing/>
+    <Route exact path ='/' component={Landing}/>
+    <section className="container">
+      <Switch>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Switch>
+    </section>
   </Fragment>
+  </Router>
 )
 
 export default App;
