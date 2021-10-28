1ed3a450f648215714bc8e68b3269d3788e0674d
Santosh Nepal <santoshnepal608@gmail.com>
2021-10-28 15:21:31 +0545

    front end cleaned up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
+import { NavBar } from './components/layout/NavBar';
+import { Landing } from './components/layout/Landing';
 import './App.css';
-import React ,{Fragment} from 'react';
-const App = () =>
+import React , {Fragment} from 'react'
+const App = () => (
   <Fragment>
+    <NavBar/>
+    <Landing/>
   </Fragment>
+)
+
 export default App;
