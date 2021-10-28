d7ae90da868de1aad65d33d4321ee34a8dfbdddd
Santosh Nepal <santoshnepal608@gmail.com>
2021-10-28 16:22:20 +0545

    login and register form created

--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,5 +1,5 @@
 import React,{Fragment,useState} from 'react'
-
+import { Link } from 'react-router-dom'
 const Register = () => {
     const [formData,setFormData] = useState({
         name:'',
@@ -8,13 +8,14 @@ const Register = () => {
         password1:''
     })
     const {name,email,password,password1} = formData
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
         if(password1!==password){
             console.log('Passord do not match')
         }
         else{
             console.log(formData)
+           
         }
     }
     return (
@@ -71,7 +72,7 @@ const Register = () => {
             <input type="submit" className="btn btn-primary" value="Register" />
         </form>
         <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
         </p>
         </Fragment>
     )
